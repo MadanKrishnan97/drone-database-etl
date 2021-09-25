1b865d0a6aafd4f2fa41a9b4878859141181ea98
arisayosh <arisayosh.kawa@gmail.com>
2021-09-25 16:02:49 -0700

    added fetch_image_ur

--- scripts/mysql_connect.py
+++ scripts/mysql_connect.py
@@ -24,3 +24,29 @@ myresult = mycursor.fetchall()
 
 for x in myresult:
   print(x)
+
+
+def fetch_image_uri(min_lat, max_lat, min_lon, max_lon):
+    mydb = mysql.connector.connect(
+        host="drone-db.cba0wbjo4ee3.us-east-2.rds.amazonaws.com",
+        user="admin",
+        password="drone123!",
+        database="bcitdrone"
+    )
+
+    mycursor = mydb.cursor()
+
+    fetch = f"SELECT * FROM gpstest " \
+            f"WHERE (latitude BETWEEN {min_lat} AND {max_lat}) AND " \
+            f"(longitude BETWEEN {min_lon} AND {max_lon})"
+
+    mycursor.execute(fetch)
+
+    myresult = mycursor.fetchall()
+
+    uris = [x[-1] for x in myresult]
+
+    return uris
+
+
+print(fetch_image_uri(0, 100, 0, 100))
