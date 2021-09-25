5421b1c70664cbd76537a1e49acf0e3b6324b88e
Niranjan Ramesh <niranjanram107@gmail.com>
2021-09-25 15:54:03 -0700

    Removed unused variables

--- scripts/flask_app.py
+++ scripts/flask_app.py
@@ -2,13 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-lat =
-url_str = f"/images/lat/{lat}/lon/{lon}"
-
 @app.route("/bcit/drone/data/get/image/longitude/from/<from_long>/to/<to_long>/latitude/from/<from_lat>/to/<to_lat>")
 def fetch_image(from_long, to_long, from_lat, to_lat):
     # fetch_from_db(from_long, to_long, from_lat, to_lat)
-    return [from_long, to_long, from_lat, to_lat]
+    return to_lat
 
 
 if __name__ == '__main__':
