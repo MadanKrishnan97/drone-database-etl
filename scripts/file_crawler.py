e214b6c125616022eba42fcde9d84edfc075e005
Echo772222 <62284054+Echo772222@users.noreply.github.com>
2021-09-25 14:41:04 -0600

    Update file_crawler.py

--- scripts/file_crawler.py
+++ scripts/file_crawler.py
@@ -59,3 +59,14 @@ if __name__ == "__main__":
     print('Other Files:')
     for fil in file_paths:
         print(fil)
+
+        
+def directory_strucutre(folder_path):
+    # getting folder structure
+    for root, dirs, files in os.walk(startpath,topdown=True):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
